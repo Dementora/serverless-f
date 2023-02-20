1c4f3f3ed35c22821ae2d61d1986e3f417aaf932
Dementora <95138659+Dementora@users.noreply.github.com>
2023-02-20 17:13:50 +0330

    Update serverless.js

--- netlify/functions/serverless.js
+++ netlify/functions/serverless.js
@@ -1,8 +1,11 @@
 exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+var list = [
+  "vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIlNBLWtpMi0yMXRoIiwNCiAgImFkZCI6ICIyMDMuMTcuMTI2LjEyNiIsDQogICJwb3J0IjogIjQ0MyIsDQogICJpZCI6ICIzYjkzZDU0My1kZDg4LTQxYTctYmFhMi03OGY0Y2IzOWYxNTIiLA0KICAiYWlkIjogIjAiLA0KICAic2N5IjogImF1dG8iLA0KICAibmV0IjogIndzIiwNCiAgInR5cGUiOiAibm9uZSIsDQogICJob3N0IjogInNlcnZlcjIuaGVxdnpscnkud29ya2Vycy5kZXYiLA0KICAicGF0aCI6ICIvc2ctZnJlZXZtZXNzLnByaXZhdGVpcC5uZXQvZnJlZXZtZXNzIiwNCiAgInRscyI6ICJ0bHMiLA0KICAic25pIjogInNlcnZlcjIuaGVxdnpscnkud29ya2Vycy5kZXYiLA0KICAiYWxwbiI6ICJoMixodHRwLzEuMSIsDQogICJmcCI6ICJyYW5kb20iDQp9",
+
+]
 
   return {
-    statusCode: 200,
-    body: `Hello, tom`
+    
+    body: btoa(list.join("\r\n"))
   };
 };
